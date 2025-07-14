69b08d0c3bc954d675029fbfd61e17d2c00ff9e6
Jeff Yanta <jeff@getcode.com>
2025-07-14 15:17:10 -0400

    Add the Profile service

--- generated/protobuf-es/index.ts
+++ generated/protobuf-es/index.ts
@@ -3,6 +3,8 @@ export * as Activity from './activity/v1';
 export * as Push from './push/v1';
 export * as Iap from './iap/v1';
 export * as Common from './common/v1';
+export * as Profile from './profile/v1';
+export * as Profile from './profile/v1';
 export * as Account from './account/v1';
 export * as Pool from './pool/v1';
 export * as Pool from './pool/v1';
